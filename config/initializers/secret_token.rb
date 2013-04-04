129f7956cec37a72decc5d8ad689cca6d85e7b28
Umrikurica <umrikurica@gmail.com>
2013-04-04 16:10:00 +0300

    dfcec

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SampleApp::Application.config.secret_token = 'c76b29498e996ab4a9f706f83ac4803725d24343134d085300244eb40e736e6382cb944de3e063b9b106cf7354b89044f1a7b78269f5fa28c09e129c257752d1'
